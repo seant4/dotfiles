1f2c8f5ac301dcf4a8a301ee3bdce512a949a96c
Sean Theisen <seanjt829@gmail.com>
2024-06-23 02:16:34 -0400

    latex 2

--- nvim/lua/seant/packer.lua
+++ nvim/lua/seant/packer.lua
@@ -7,9 +7,9 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use "nvim-lua/plenary.nvim"
-  use "lervag/vimtex"
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use 'jalvesaq/Nvim-R'
+  use 'vim-latex/vim-latex'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
 	  -- or                            , branch = '0.1.x',
